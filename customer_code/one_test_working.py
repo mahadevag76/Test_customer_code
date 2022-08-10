2a71bc55586f1a0f883c0b9527b5c81409a3e309
mahadevag76 <89843961+mahadevag76@users.noreply.github.com>
2022-08-10 16:48:22 +0530

    updated

--- customer_code/one_test_working.py
+++ customer_code/one_test_working.py
@@ -18,13 +18,10 @@ class my_sample_test(BaseClass):
 
     @BaseClass.test(tags=[["sanity_tc1"]])
     def test_case_1(self):
-<<<<<<< HEAD
         self.logger.info("print from TestCase 1")
-        self.logger.info("-----------------")
-=======
+        self.logger.info("-----------")
         self.logger.info("print from the TestCase1")
         self.logger.info("----------------------------")
->>>>>>> f0774ed12f015abbd86536c3f049bb68563f5b94
         subprocess.Popen('cd ../../customer_code; make', shell=True)
         subprocess.Popen('cd ../../customer_code; make clean', shell=True)
     
